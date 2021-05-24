1d85616a0c64cd2ed24939c1fc5cfc7b02c6a8f7
sunil poudel <sunil15poudel>
2021-05-24 19:40:08 +0545

    use Go slice data type,  Return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
     "errors"
     "fmt"
+    "math/rand"
+    "time"
 )
 
 func Hello(name string) (string, error) {
@@ -11,6 +13,26 @@ func Hello(name string) (string, error) {
         return "", errors.New("Empty name")
     }
 
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf( randomFormat(), name)  //"Hi, %v. Welcome!"
     return message, nil
+}
+
+// Go executes init functions automatically at program startup, 
+// after global variables have been initialized
+func init(){
+    rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string{
+
+    // slice data type => like arry 
+    // but its size change dynamically 
+    // on inserting & deleting record
+    formats := []string{
+        "Hi %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    return formats[rand.Intn(len(formats))]
 }
